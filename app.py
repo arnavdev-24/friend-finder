b6be1a887a0090dedb0f503953d2f0116836c687
arnavsaxena24 <arnavsaxenaofficial.24@gmail.com>
2026-09-12 12:48:48 +0530

    added results html

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 from matcher import get_top_matches
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Server is running"
+    return "Server Running"
 
-@app.route("/matches/<int:user_id>")
-def matches(user_id):
-    results = get_top_matches(user_id)
-    formatted=[]
-    for name,score,reasons in results:
+@app.route("/results/<int:user_id>")
+def results(user_id):
+    matches = get_top_matches(user_id)
+
+    formatted = []
+    for name, score, reasons in matches:
         formatted.append({
             "name": name,
             "score": score,
             "reasons": reasons
         })
-    return jsonify(formatted)
+
+    return render_template("results.html", matches=formatted)
 
 
 if __name__ == "__main__":
