18d0731276a1c7302d44a1b4a630b218398d9723
arnavsaxena24 <arnavsaxenaofficial.24@gmail.com>
2026-09-13 00:43:31 +0530

    save before pull

--- matcher.py
+++ matcher.py
@@ -1,12 +1,13 @@
 import mysql.connector
+import os
 def get_connection():
     return mysql.connector.connect(
 
-
-        host="localhost",
-        user="root",
-        password="root123",
-        database="friend_finder"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME"),
+        port=os.getenv("DB_PORT")
 )
 
 
